4e4a28272427e0bd670236883b2800148f1269ef
Alex <zs22017012@estudiantes.uv.mx>
2025-06-01 00:43:09 -0600

    Creado la parte de comentarios

---
+++ foroLIS backend/DTOs/CommentDtos/CommentDto.cs
@@ -0,0 +1,14 @@
+﻿namespace foroLIS_backend.DTOs.CommentDtos;
+
+public class CommentDto
+{
+    public Guid Id { get; set; }
+    public string ArchitectureOS { get; set; }
+    public string FamilyOS { get; set; }
+    public string VersionOS { get; set; }
+    public Guid PostId { get; set; }
+    public string UserId { get; set; }
+    public Guid? MediaFileId { get; set; }
+    public DateTime CreateAt { get; set; }
+    public DateTime UpdateAt { get; set; }
+}
