fdc633553b8ea52524a4b6865b5809a1363043c0
Firepants69 <rafaelmerlinprieto@gmail.com>
2025-06-04 21:03:55 -0600

    xd

--- foroLIS backend/Validators/CreateSurveyValidator.cs
+++ foroLIS backend/Validators/CreateSurveyValidator.cs
@@ -8,7 +8,7 @@ namespace foroLIS_backend.Validators
         public CreateSurveyValidator() {
             RuleFor(ccs => ccs.EndDate).Must(date => date >= DateTime.UtcNow).WithMessage("la fecha de vencimiento de la encuesta tiene que ser despues de la fecha actual");
             RuleFor(css => css.AllowMoreOneAnswer).NotEmpty();
-            RuleFor(css => css.Fields).Must(f => f.Count() > 2).WithMessage("la encuesta debe de tener al menos 2 campos");
+            RuleFor(css => css.Fields).Must(f => f.Count() >= 2).WithMessage("la encuesta debe de tener al menos 2 campos");
             RuleFor(css => css.Title).Length(1, 256);
         }
     }
